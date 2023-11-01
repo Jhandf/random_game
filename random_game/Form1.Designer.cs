91d6f65885089378a477c0be36f59016db435c74
mimhle <40392166+mimhle@users.noreply.github.com>
2023-11-01 20:43:56 +0700

    Initial commit

---
+++ random_game/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace random_game {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSpin = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtTokensCount = new System.Windows.Forms.Label();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            //
+            // btn1
+            //
+            this.btn1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(12, 64);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(120, 94);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "0";
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.btn_Click);
+            //
+            // btn2
+            //
+            this.btn2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.Location = new System.Drawing.Point(138, 64);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(120, 94);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "0";
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.btn_Click);
+            //
+            // btn3
+            //
+            this.btn3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btn3.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.Location = new System.Drawing.Point(264, 64);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(120, 94);
+            this.btn3.TabIndex = 2;
+            this.btn3.Text = "0";
+            this.btn3.UseVisualStyleBackColor = false;
+            this.btn3.Click += new System.EventHandler(this.btn_Click);
+            //
+            // btn6
+            //
+            this.btn6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btn6.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn6.Location = new System.Drawing.Point(264, 164);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(120, 94);
+            this.btn6.TabIndex = 5;
+            this.btn6.Text = "0";
+            this.btn6.UseVisualStyleBackColor = false;
+            this.btn6.Click += new System.EventHandler(this.btn_Click);
+            //
+            // btn5
+            //
+            this.btn5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btn5.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.Location = new System.Drawing.Point(138, 164);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(120, 94);
+            this.btn5.TabIndex = 4;
+            this.btn5.Text = "0";
+            this.btn5.UseVisualStyleBackColor = false;
+            this.btn5.Click += new System.EventHandler(this.btn_Click);
+            //
+            // btn4
+            //
+            this.btn4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btn4.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.Location = new System.Drawing.Point(12, 164);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(120, 94);
+            this.btn4.TabIndex = 3;
+            this.btn4.Text = "0";
+            this.btn4.UseVisualStyleBackColor = false;
+            this.btn4.Click += new System.EventHandler(this.btn_Click);
+            //
+            // label1
+            //
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(372, 52);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Pick your animal or sth";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            //
+            // btnSpin
+            //
+            this.btnSpin.Location = new System.Drawing.Point(138, 279);
+            this.btnSpin.Name = "btnSpin";
+            this.btnSpin.Size = new System.Drawing.Size(120, 47);
+            this.btnSpin.TabIndex = 7;
+            this.btnSpin.Text = "Spin!!!";
+            this.btnSpin.UseVisualStyleBackColor = true;
+            this.btnSpin.Click += new System.EventHandler(this.btnSpin_Click);
+            //
+            // textBox1
+            //
+            this.textBox1.Location = new System.Drawing.Point(12, 370);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(120, 26);
+            this.textBox1.TabIndex = 8;
+            //
+            // label2
+            //
+            this.label2.Location = new System.Drawing.Point(12, 344);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 23);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Your pocket:";
+            //
+            // label3
+            //
+            this.label3.Location = new System.Drawing.Point(138, 344);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 23);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Your pocket:";
+            //
+            // textBox2
+            //
+            this.textBox2.Location = new System.Drawing.Point(138, 370);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(120, 26);
+            this.textBox2.TabIndex = 10;
+            //
+            // label4
+            //
+            this.label4.Location = new System.Drawing.Point(264, 344);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 23);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Your pocket:";
+            //
+            // textBox3
+            //
+            this.textBox3.Location = new System.Drawing.Point(264, 370);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(120, 26);
+            this.textBox3.TabIndex = 12;
+            //
+            // txtTokensCount
+            //
+            this.txtTokensCount.Location = new System.Drawing.Point(12, 292);
+            this.txtTokensCount.Name = "txtTokensCount";
+            this.txtTokensCount.Size = new System.Drawing.Size(120, 23);
+            this.txtTokensCount.TabIndex = 14;
+            this.txtTokensCount.Text = "Tokens left: 3";
+            //
+            // txtScore
+            //
+            this.txtScore.Location = new System.Drawing.Point(264, 292);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(120, 23);
+            this.txtScore.TabIndex = 15;
+            this.txtScore.Text = "Your score: 0";
+            //
+            // Form1
+            //
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 415);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.txtTokensCount);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnSpin);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Label txtScore;
+
+        private System.Windows.Forms.Label txtTokensCount;
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+
+        private System.Windows.Forms.Button btnSpin;
+
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Label label1;
+
+        private System.Windows.Forms.Button btn1;
+
+        #endregion
+    }
+}
---
+++ random_game/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Drawing;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace random_game {
+    public partial class Form1 : Form {
+        private readonly Timer _rollTimer = new Timer();
+        private int _totalTicks = 100;
+        private bool _finishRoll;
+        private readonly Button[] _buttons = new Button[6];
+        private readonly Color[] _initialColors = new Color[6];
+
+        public Form1() {
+            InitializeComponent();
+
+            _rollTimer.Interval = 10;
+            _rollTimer.Tick += timer_Tick;
+
+            Button[] buttons = { btn1, btn2, btn3, btn4, btn5, btn6 };
+            for (int i = 0; i < buttons.Length; i++) {
+                _buttons[i] = buttons[i];
+                _initialColors[i] = buttons[i].BackColor;
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e) {
+            var rolls = updateRolls();
+            for (int i = 0; i < _buttons.Length; i++) {
+                if (i != int.Parse(rolls[0].ToString()) - 1 && i != int.Parse(rolls[2].ToString()) - 1 &&
+                    i != int.Parse(rolls[4].ToString()) - 1) {
+                    setBtnColor(_buttons[i], Color.Gray);
+                } else {
+                    setBtnColor(_buttons[i], _initialColors[i]);
+                }
+            }
+            Regex rePattern = new Regex(@"(?<=:\s)*\d+");
+            txtScore.Text = rePattern.Replace(txtScore.Text, calcScore(rolls).ToString());
+            _totalTicks--;
+            _rollTimer.Interval += 1;
+            if (_totalTicks <= 0) {
+                for (int i = 0; i < _buttons.Length; i++) {
+                    if (i == int.Parse(rolls[0].ToString()) - 1 && i == int.Parse(rolls[2].ToString()) - 1 &&
+                        i == int.Parse(rolls[4].ToString()) - 1) {
+                        blinkBtn(i);
+                    }
+                }
+                _rollTimer.Stop();
+                _totalTicks = 100;
+                _rollTimer.Interval = 10;
+                _finishRoll = true;
+            }
+        }
+
+        private void btn_Click(object sender, EventArgs e) {
+            Button btn = (Button)sender;
+            int currentNumber = int.Parse(btn.Text);
+
+            Regex rePattern = new Regex(@"(?<=:\s)*\d+");
+            if (rePattern.Match(txtTokensCount.Text).Value != @"0" && _finishRoll == false) {
+                txtTokensCount.Text = rePattern.Replace(txtTokensCount.Text, (int.Parse(rePattern.Match(txtTokensCount.Text).Value) - 1).ToString());
+                btn.Text = (currentNumber + 1).ToString();
+            } else if (_finishRoll) {
+                resetBtns();
+                _finishRoll = false;
+                txtTokensCount.Text = rePattern.Replace(txtTokensCount.Text, @"2");
+                btn.Text = @"1";
+            }
+        }
+
+        private void btnSpin_Click(object sender, EventArgs e) {
+            if (_finishRoll) {
+                return;
+            }
+            _rollTimer.Start();
+        }
+
+        private string updateRolls() {
+            Random rnd = new Random();
+            int randomNumber1 = rnd.Next(1, 6);
+            int randomNumber2 = rnd.Next(1, 6);
+            int randomNumber3 = rnd.Next(1, 6);
+            btnSpin.Text = randomNumber1 + @" " + randomNumber2 + @" " + randomNumber3;
+            return btnSpin.Text;
+        }
+
+        private void resetBtns() {
+            for (int i = 0; i < _buttons.Length; i++) {
+                _buttons[i].Text = @"0";
+                setBtnColor(_buttons[i], _initialColors[i]);
+            }
+        }
+
+        private int calcScore(string rolls) {
+            int[] numbers = { int.Parse(rolls[0].ToString()), int.Parse(rolls[2].ToString()), int.Parse(rolls[4].ToString()) };
+            int score = 0;
+            for (int i = 0; i < _buttons.Length; i++) {
+                if (numbers[0] == i + 1 && numbers[1] == i + 1 && numbers[2] == i + 1) {
+                    score += int.Parse(_buttons[i].Text) * 3;
+                } else if (numbers[0] == i + 1 && numbers[1] == i + 1) {
+                    score += int.Parse(_buttons[i].Text) * 2;
+                } else if (numbers[0] == i + 1 && numbers[2] == i + 1) {
+                    score += int.Parse(_buttons[i].Text) * 2;
+                } else if (numbers[1] == i + 1 && numbers[2] == i + 1) {
+                    score += int.Parse(_buttons[i].Text) * 2;
+                } else if (numbers[0] == i + 1) {
+                    score += int.Parse(_buttons[i].Text);
+                } else if (numbers[1] == i + 1) {
+                    score += int.Parse(_buttons[i].Text);
+                } else if (numbers[2] == i + 1) {
+                    score += int.Parse(_buttons[i].Text);
+                }
+            }
+            return score;
+        }
+
+        private void setBtnColor(Button btn, Color color) {
+            btn.BackColor = color;
+        }
+
+        private void blinkBtn(int index) {
+            Timer blinkTimer = new Timer();
+            blinkTimer.Interval = 500;
+            int ticks = 10;
+            blinkTimer.Tick += (sender, args) => {
+                if (ticks <= 0) {
+                    blinkTimer.Stop();
+                    setBtnColor(_buttons[index], _initialColors[index]);
+                } else {
+                    setBtnColor(_buttons[index],
+                        _buttons[index].BackColor == Color.Gray ? _initialColors[index] : Color.Gray);
+                    ticks--;
+                }
+            };
+            blinkTimer.Start();
+        }
+    }
+}
