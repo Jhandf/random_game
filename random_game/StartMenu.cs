bf16129a73aae38521fa80d8806f9fe05cc7dc1d
mimhle <40392166+mimhle@users.noreply.github.com>
2023-11-04 10:22:59 +0700

    Added StartMenu template

---
+++ random_game/StartMenu.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Forms;
+
+namespace random_game {
+    public partial class StartMenu : Form {
+        public StartMenu() {
+            InitializeComponent();
+        }
+    }
+}
