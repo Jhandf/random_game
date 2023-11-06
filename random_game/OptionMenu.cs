8a65a6c4813f3cdc5390f4c55bc57df90a7c619e
mimhle <40392166+mimhle@users.noreply.github.com>
2023-11-06 15:44:57 +0700

    Refactor code

--- random_game/OptionMenu.cs
+++ random_game/OptionMenu.cs
@@ -32,13 +32,13 @@ namespace random_game {
                 StartMenu.player.settings.volume = 0;
                 label1.Text = @"0%";
                 pictureBox1.Image = Resources.sound_off;
-            } else {
-                trackBar1.Value = Program.prevVolumeValue;
-                Program.volume = Program.prevVolumeValue;
-                StartMenu.player.settings.volume = Program.prevVolumeValue;
-                label1.Text = Program.prevVolumeValue + @"%";
-                pictureBox1.Image = Resources.sound_on;
+                return;
             }
+            trackBar1.Value = Program.prevVolumeValue;
+            Program.volume = Program.prevVolumeValue;
+            StartMenu.player.settings.volume = Program.prevVolumeValue;
+            label1.Text = Program.prevVolumeValue + @"%";
+            pictureBox1.Image = Resources.sound_on;
         }
     }
 }
