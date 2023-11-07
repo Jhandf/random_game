1a84bc96c4305aafac926ed9a2bbe499288fe522
mimhle <40392166+mimhle@users.noreply.github.com>
2023-11-07 09:32:57 +0700

    Fixed form size

--- random_game/MainGame.Designer.cs
+++ random_game/MainGame.Designer.cs
@@ -38,19 +38,19 @@
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(11, 7);
+            this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(475, 42);
+            this.label1.Size = new System.Drawing.Size(534, 52);
             this.label1.TabIndex = 6;
             this.label1.Text = "Pick your animal or sth";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // btnSpin
             // 
-            this.btnSpin.Location = new System.Drawing.Point(171, 267);
+            this.btnSpin.Location = new System.Drawing.Point(192, 334);
             this.btnSpin.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnSpin.Name = "btnSpin";
-            this.btnSpin.Size = new System.Drawing.Size(155, 61);
+            this.btnSpin.Size = new System.Drawing.Size(174, 76);
             this.btnSpin.TabIndex = 7;
             this.btnSpin.Text = "Spin!!!";
             this.btnSpin.UseVisualStyleBackColor = true;
@@ -58,18 +58,18 @@
             // 
             // txtTokensCount
             // 
-            this.txtTokensCount.Location = new System.Drawing.Point(11, 267);
+            this.txtTokensCount.Location = new System.Drawing.Point(12, 334);
             this.txtTokensCount.Name = "txtTokensCount";
-            this.txtTokensCount.Size = new System.Drawing.Size(155, 61);
+            this.txtTokensCount.Size = new System.Drawing.Size(174, 76);
             this.txtTokensCount.TabIndex = 14;
             this.txtTokensCount.Text = "Tokens:";
             this.txtTokensCount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // txtScore
             // 
-            this.txtScore.Location = new System.Drawing.Point(331, 267);
+            this.txtScore.Location = new System.Drawing.Point(372, 334);
             this.txtScore.Name = "txtScore";
-            this.txtScore.Size = new System.Drawing.Size(155, 61);
+            this.txtScore.Size = new System.Drawing.Size(174, 76);
             this.txtScore.TabIndex = 15;
             this.txtScore.Text = "Your score:";
             this.txtScore.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -80,10 +80,10 @@
             this.btn6.BackgroundImage = global::random_game.Properties.Resources.kokomi;
             this.btn6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btn6.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn6.Location = new System.Drawing.Point(331, 159);
+            this.btn6.Location = new System.Drawing.Point(372, 199);
             this.btn6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn6.Name = "btn6";
-            this.btn6.Size = new System.Drawing.Size(155, 103);
+            this.btn6.Size = new System.Drawing.Size(174, 129);
             this.btn6.TabIndex = 5;
             this.btn6.UseVisualStyleBackColor = false;
             this.btn6.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btn_MouseUp);
@@ -95,10 +95,10 @@
             this.btn5.BackgroundImage = global::random_game.Properties.Resources.nilou;
             this.btn5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btn5.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn5.Location = new System.Drawing.Point(171, 159);
+            this.btn5.Location = new System.Drawing.Point(192, 199);
             this.btn5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn5.Name = "btn5";
-            this.btn5.Size = new System.Drawing.Size(155, 103);
+            this.btn5.Size = new System.Drawing.Size(174, 129);
             this.btn5.TabIndex = 4;
             this.btn5.UseVisualStyleBackColor = false;
             this.btn5.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btn_MouseUp);
@@ -110,10 +110,10 @@
             this.btn4.BackgroundImage = global::random_game.Properties.Resources.raiden;
             this.btn4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btn4.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn4.Location = new System.Drawing.Point(11, 159);
+            this.btn4.Location = new System.Drawing.Point(12, 199);
             this.btn4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn4.Name = "btn4";
-            this.btn4.Size = new System.Drawing.Size(155, 103);
+            this.btn4.Size = new System.Drawing.Size(174, 129);
             this.btn4.TabIndex = 3;
             this.btn4.UseVisualStyleBackColor = false;
             this.btn4.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btn_MouseUp);
@@ -125,10 +125,10 @@
             this.btn3.BackgroundImage = global::random_game.Properties.Resources.zongli;
             this.btn3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btn3.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn3.Location = new System.Drawing.Point(331, 51);
+            this.btn3.Location = new System.Drawing.Point(372, 64);
             this.btn3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn3.Name = "btn3";
-            this.btn3.Size = new System.Drawing.Size(155, 103);
+            this.btn3.Size = new System.Drawing.Size(174, 129);
             this.btn3.TabIndex = 2;
             this.btn3.UseVisualStyleBackColor = false;
             this.btn3.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btn_MouseUp);
@@ -140,10 +140,10 @@
             this.btn2.BackgroundImage = global::random_game.Properties.Resources.neuvillete;
             this.btn2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn2.Location = new System.Drawing.Point(171, 51);
+            this.btn2.Location = new System.Drawing.Point(192, 64);
             this.btn2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn2.Name = "btn2";
-            this.btn2.Size = new System.Drawing.Size(155, 103);
+            this.btn2.Size = new System.Drawing.Size(174, 129);
             this.btn2.TabIndex = 1;
             this.btn2.UseVisualStyleBackColor = false;
             this.btn2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btn_MouseUp);
@@ -155,10 +155,10 @@
             this.btn1.BackgroundImage = global::random_game.Properties.Resources.wanderer;
             this.btn1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn1.Location = new System.Drawing.Point(11, 51);
+            this.btn1.Location = new System.Drawing.Point(12, 64);
             this.btn1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn1.Name = "btn1";
-            this.btn1.Size = new System.Drawing.Size(155, 103);
+            this.btn1.Size = new System.Drawing.Size(174, 129);
             this.btn1.TabIndex = 0;
             this.btn1.UseVisualStyleBackColor = false;
             this.btn1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btn_MouseUp);
@@ -166,9 +166,9 @@
             // 
             // MainGame
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(497, 353);
+            this.ClientSize = new System.Drawing.Size(559, 441);
             this.Controls.Add(this.txtScore);
             this.Controls.Add(this.txtTokensCount);
             this.Controls.Add(this.btnSpin);
@@ -179,11 +179,13 @@
             this.Controls.Add(this.btn3);
             this.Controls.Add(this.btn2);
             this.Controls.Add(this.btn1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "MainGame";
             this.Text = "Form1";
             this.ResumeLayout(false);
-
         }
 
         private System.Windows.Forms.Label txtScore;
