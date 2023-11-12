d0280a0bcfe46840b551402c0693c48d249c1449
Jhandf <nguyenduy2004.d@gmail.com>
2023-11-12 10:08:53 +0700

    Add GiftCode

--- random_game/EnterGiftCode.cs
+++ random_game/EnterGiftCode.cs
@@ -11,21 +11,26 @@ using System.Windows.Forms;
 namespace random_game {
     public partial class EnterGiftCode : Form {
 
-        string strGift0 = "TTT";
-        string strGift1 = "TS";
+        private static readonly string[] strGift = new string[] { "9T96KJNE2LVM", "NTQP2KPEJMUH", "VA97KJNF24UV", "ZT8MLL8GCYKM", "WA845MQHUHKH", "3BRLL59ZCZKD" };
+        private bool[] _activate = new bool[strGift.Length];
+
         public event Action<int> tokenCountUpdated;
 
         public EnterGiftCode() {
             InitializeComponent();
+            for (int i = 0; i < strGift.Length; i++)
+                _activate[i] = false;
         }
 
         private void btnSubmit_Click(object sender, EventArgs e) {
             var newTokenCount = 100;
-            if (strGift0 == txtGiftCode.Text || strGift1 == txtGiftCode.Text) {
-                tokenCountUpdated?.Invoke(newTokenCount);
-                txtGiftCode.Text = "";
-                txtGiftCode.Focus();
-            }
+            for (int i = 0; i <= strGift.Length; i++)
+                if (strGift[i] == txtGiftCode.Text) {
+                    _activate[i] = true;
+                    tokenCountUpdated?.Invoke(newTokenCount);
+                    txtGiftCode.Text = "";
+                    txtGiftCode.Focus();
+                }
         }
 
     }
